6d523eaa776689d03cceae672993754fd5068825
dimakruhlyi <dimakruhly@gmail.com>
2020-04-22 15:33:59 +0300

    add Header and Footer Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './components/MainComponent';
+import './App.css'
 
 class App extends Component {
 
